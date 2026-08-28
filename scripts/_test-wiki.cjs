f16a42d13bc8f02054187078642264e065a64036
antoniocan2022-dev <antonio.can2022@hotmail.com>
2026-08-28 18:00:31 -0400

    remove: obsolete scratch wiki test

--- scripts/_test-wiki.cjs
+++
@@ -1,6 +0,0 @@
-const { PrismaClient } = require('@prisma/client')
-async function main() {
-  const { toolWikipediaSearch, toolFreeApisDirectory } = require('/home/z/my-project/.next/server/chunks/_lib_tools_ts.js')
-  console.log('wiki:', Object.keys({ toolWikipediaSearch, toolFreeApisDirectory }))
-}
-main().catch(e => console.error(e))
